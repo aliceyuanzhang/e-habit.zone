f9165db8ad0a59c2b94fc2b258f324e946b63513
Tong <tongwu226@gmail.com>
2021-03-01 02:15:02 -0500

    update UI design for open garden
    etherpad embedded

--- js/main.js
+++ js/main.js
@@ -8,30 +8,71 @@ function closeAbout() {
 };
 
 
+var firstButtonForm = document.getElementById('at-form')
 var firstButtonInput = document.getElementById('at-input');
+var firstButtonIcon = document.getElementById('at-input-send');
 var restButtons = document.getElementsByClassName("restButtons");
 var backButton = document.getElementById('choice-button-back');
 
+//alt-text-section
 function expandAltText(e) {
+    firstButtonForm.classList.replace('dp-none', 'dp-flex');
     firstButtonInput.classList.replace('alt-text-input', 'alt-text-input-visible');
+    firstButtonIcon.classList.replace('dp-none', 'dp-block');
     for (var i = 0; i < restButtons.length; i += 1) {
         restButtons[i].style.display = 'none';
     }
     backButton.style.display = "block";
     window.setTimeout(function () {
-        firstButtonInput.classList.toggle('expandWidth');
+        firstButtonInput.classList.add('expandWidth');
     }, 20)
 };
 
+function pushAltText() {
+    alert("you clicked me!")
+}
+
+var etherPadContent = document.getElementById('pop-up-add-instruction');
+
+function showEtherpad() {
+    //show add-instruction content
+    etherPadContent.classList.replace('dp-none', 'dp-block');
+    //hide all the other buttons and show "back"
+    for (var i = 0; i < restButtons.length; i += 1) {
+        restButtons[i].style.display = 'none';
+    }
+    backButton.style.display = "block";
+}
+
+var reminderContent = document.getElementById('pop-up-set-reminder');
+
+function setReminder() {
+    //show reminder content
+    reminderContent.classList.replace('dp-none', 'dp-block');
+    //hide all the other buttons and show "back"
+    for (var i = 0; i < restButtons.length; i += 1) {
+        restButtons[i].style.display = 'none';
+    }
+    backButton.style.display = "block";
+}
+
+function submitReminder() {
+    alert("you submitted me!")
+}
+
+//back button
 
 function backMain() {
-    window.setTimeout(function () {
-        firstButtonInput.classList.toggle('expandWidth');
-    }, 20);
+    //hide alt-text functions
+    firstButtonInput.classList.remove('expandWidth');
+    firstButtonForm.classList.replace('dp-flex', 'dp-none');
     firstButtonInput.classList.replace('alt-text-input-visible', 'alt-text-input');
+    firstButtonIcon.classList.replace('dp-block', 'dp-none');
+    //hide add-instruction content
+    etherPadContent.classList.replace('dp-block', 'dp-none');
+
     for (var i = 0; i < restButtons.length; i += 1) {
         restButtons[i].style.display = 'block';
     }
     backButton.style.display = "none";
-
 }
